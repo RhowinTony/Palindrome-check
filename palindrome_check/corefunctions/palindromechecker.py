03f825d7681f28dab519354b16b96a8cad7a717a
Rhowin Tony <mail2rhowintony.j@gmail.com>
2022-07-19 23:58:15 +0530

    final commit

--- palindrome_check/corefunctions/palindromechecker.py
+++ palindrome_check/corefunctions/palindromechecker.py
@@ -4,41 +4,41 @@
 #     is_palindrome = palindrome(filtered_data)
 # """
 
-# class Palindromecheck:
-#     def __init__(self, inp_val:str) -> None:
-#         try:
-#             self.givenstring = inp_val.lower()
-#         except KeyboardInterrupt:
-#             print("\n Program interruptd by user")            
+class Palindromecheck:
+    def __init__(self, inp_val:str) -> None:
+        try:
+            self.givenstring = inp_val.lower()
+        except KeyboardInterrupt:
+            print("\n Program interruptd by user")            
 
   
-#     def str_filter(self):
-#         str_filters = "abcdefghijklmnopqrstuvwxyz1234567890"
-#         filtered_data = ""
-#         for i in self.givenstring:
-#             if i in str_filters:
-#                 filtered_data = filtered_data + i
-#         return filtered_data
+    def str_filter(self):
+        str_filters = "abcdefghijklmnopqrstuvwxyz1234567890"
+        filtered_data = ""
+        for i in self.givenstring:
+            if i in str_filters:
+                filtered_data = filtered_data + i
+        return filtered_data
 
-#     def reverse_check(self,value):
-#         i = 0
-#         length = len(value)
-#         inpu = list(value)
-#         output = []
-#         while length > 0 :
-#             i = i - 1
-#             length = length - 1
-#             output.append(value[i])
-#         return output == inpu
+    def reverse_check(self,value):
+        i = 0
+        length = len(value)
+        inpu = list(value)
+        output = []
+        while length > 0 :
+            i = i - 1
+            length = length - 1
+            output.append(value[i])
+        return output == inpu
 
-#     def get_result(self):
-#         filtered_dat = self.str_filter()
-#         is_palindrome = self.reverse_check(filtered_dat)
-#         if is_palindrome:
-#             print("The given string is a palindrome")
-#         else:
-#             print("The given string is not a palindrome")
-#         print("_*___*_"*6)
+    def get_result(self):
+        filtered_dat = self.str_filter()
+        is_palindrome = self.reverse_check(filtered_dat)
+        if is_palindrome:
+            print("The given string is a palindrome")
+        else:
+            print("The given string is not a palindrome")
+        print("_*___*_"*6)
 
 
 
